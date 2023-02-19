3b26383b96b7c16c7dff82cecabce4fc1bcf807a
Hanger12 <killersuper1212@gmail.com>
2023-02-19 19:38:18 +0400

    test

--- src/main/java/com/project/calculate/CalculateApplication.java
+++ src/main/java/com/project/calculate/CalculateApplication.java
@@ -8,7 +8,7 @@ public class CalculateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculateApplication.class, args);
-		//comment sadsadsadsadsadsdasds       dsadsadsdadasd
+		//comment
 	}
 
 }
