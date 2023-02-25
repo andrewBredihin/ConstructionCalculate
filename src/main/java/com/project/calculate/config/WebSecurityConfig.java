255a970971be993d2e242fc76d921c0a6377c43a
Андрей Бредихин <102852334+andrewBredihin@users.noreply.github.com>
2023-02-25 20:46:21 +0400

    Repair entity

--- src/main/java/com/project/calculate/config/WebSecurityConfig.java
+++ src/main/java/com/project/calculate/config/WebSecurityConfig.java
@@ -26,7 +26,6 @@ public class WebSecurityConfig {
                         .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
-
         return http.build();
     }
 
