ffa75e65ed674fe58396ea91d6148fe710cb11a3
Hanger12 <killersuper1212@gmail.com>
2023-03-12 22:13:40 +0400

    Страница Расчета Complete on 60%

--- src/main/java/com/project/calculate/config/MvcConfig.java
+++ src/main/java/com/project/calculate/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.project.calculate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,8 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/addClient").setViewName("addClient");
         registry.addViewController("/editClient").setViewName("editClient");
         registry.addViewController("/skeletonPage").setViewName("skeletonPage");
+        registry.addViewController("/calculation").setViewName("calculation");
         registry.addViewController("/login").setViewName("login");
     }
+
 }
