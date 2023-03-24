fc56ecf20866e81073c5a1ded10616262da5617a
Андрей Бредихин <102852334+andrewBredihin@users.noreply.github.com>
2023-03-24 19:57:05 +0400

    Починил отображение картники

--- src/main/java/com/project/calculate/config/WebSecurityConfig.java
+++ src/main/java/com/project/calculate/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers("/resources/**").permitAll()
+                .requestMatchers("/resources/**", "/images/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
